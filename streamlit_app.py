e149deec90de112b463602c404a1ab1e4966a2f5
jlmiller12s <jlmiller12s@gmail.com>
2025-10-23 09:49:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,19 @@
 import streamlit as st
-import requests
+import pandas as pd
+import numpy as np
 
-st.title("API Test – Streamlit Sandbox")
+st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
 
-if st.button("Fetch Tasks"):
-    r = requests.get("https://jsonplaceholder.typicode.com/todos")
-    data = r.json()[:10]  # limit for demo
-    st.write("✅ Success! Showing sample data:")
-    st.dataframe(data)
+all_users = ["Alice", "Bob", "Charly"]
+with st.container(border=True):
+    users = st.multiselect("Users", all_users, default=all_users)
+    rolling_average = st.toggle("Rolling average")
+
+np.random.seed(42)
+data = pd.DataFrame(np.random.randn(20, len(users)), columns=users)
+if rolling_average:
+    data = data.rolling(7).mean().dropna()
+
+tab1, tab2 = st.tabs(["Chart", "Dataframe"])
+tab1.line_chart(data, height=250)
+tab2.dataframe(data, height=250, use_container_width=True)
